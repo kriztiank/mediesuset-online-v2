9edcea352c80bfb9326f8a7ebddf255f498e574c
kriztiank <kriztiank@gmail.com>
2020-06-24 08:57:48 +0200

    stageDay  ;files created

---
+++ frontend/src/pages/wednesday.js
@@ -0,0 +1,47 @@
+import React from "react"
+import Layout from "../components/Layout"
+import { graphql } from "gatsby"
+import Artists from "../components/Artists"
+import SEO from "../components/SEO"
+
+// with page query you can access graphql in the data object
+// artists holds the data, pass to components/Artists.js plural
+export default ({ data }) => {
+  // console.log(data)
+  // destructuring the graphql data
+  // rename nodes to artists with alias
+  const {
+    allStrapiArtists: { nodes: artists },
+  } = data
+
+  return (
+    <Layout>
+      <SEO title="Artists" />
+      <Artists artists={artists} title="LINEUP" />
+    </Layout>
+  )
+}
+
+// Filter stageName equal to blue
+export const query = graphql`
+  {
+  allStrapiArtists(filter: {stageDay: {eq: "Onsdag"}}) {
+    nodes {
+      id
+      slug
+      name
+      stageTime
+      stageName
+      stageDay
+      image {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  }
+}
+`
+// ...GatsbyImageSharpFluid
